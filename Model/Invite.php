d45a7b45e1a52a707213fb71899f9671aed00832
Yingjun Cui <yingjun@buildrr.com>
2014-01-22 14:24:29 -0500

    add method for chage status column

--- Model/Invite.php
+++ Model/Invite.php
@@ -28,4 +28,45 @@ class Invite extends InvitesAppModel {
 		)
 	);
 
+
+
+
+	private function _updateStatus($inviteId,$status,$userId = null){
+		$row = $this->read(array('status'),$inviteId);
+
+
+
+		if(!empty($row) && $row['Invite']['status'] <= 0){
+			$data = array(
+				'Invite'=>array(
+					'status'=> $status,
+					'user_id'=>$userId,
+				),
+			);
+			return $this->save($data);
+		}
+
+		return false;
+	}
+
+	public function accept($inviteId,$userId = null){
+		$this->_updateStatus($inviteId,1,$userId);
+	}
+
+	public function decline($inviteId,$userId = null){
+		$this->_updateStatus($inviteId,-1,$userId);
+	}
+
+	public function findAllAcceptUserIdByTaskId($taskId,$creator_id){
+		return $this->find('all',
+			array(
+				'conditions'=>array('Invite.foreign_key'=>$taskId,
+					'Invite.status'=>1,'Invite.creator_id'=>$creator_id,
+					'Invite.user_id !='=> null,
+					'Invite.user_id !='=> $creator_id,
+				),
+
+			));
+	}
+
 }
